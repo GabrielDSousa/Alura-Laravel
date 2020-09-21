755fd6ce65f04270457c533e760bb391fab4f2a7
Gabriel Ramos de Sousa <gabrielramos.email@gmail.com>
2020-09-21 09:53:53 -0300

    Fifth class:migration of a column, checkboxes array,usage redirect->back, id to object direct by laravel, include sub views

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::post('/series/adicionar', 'SeriesController@store');
 Route::delete('/series/{id}', 'SeriesController@destroy');
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
+Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
